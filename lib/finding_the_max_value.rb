5f900d69ce8b6f7f297dda79cb6e3d9a745cf132
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-06 13:48:52 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,12 @@
 # def find_max_value(array)
 #   array.sort![-1]
 # end
+
+def find_max_value(array)
+  max = 0
+  counter = 0
+  
+  while array.length > counter do
+    if array[counter] > i
+  end
+end
